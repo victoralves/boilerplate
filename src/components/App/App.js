b30d960d7e357d9701a84e9ad532839b4b9d9c62
rlage <rafael.tavares@mprj.mp.br>
2018-01-17 11:22:02 -0200

    Correct indentation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,19 +11,19 @@ import appReducer from '../../reducers/appReducer.js'
 const store = createStore(appReducer, applyMiddleware(logger))
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    </Provider>
-  );
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
