fbf82a3a00b7aec3d12e07dc6d8589d3f1e76647
rlage <rafael.tavares@mprj.mp.br>
2018-01-22 14:16:03 -0200

    Add google analytics init function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import './index.css'
 import App from './components/App/App.js'
 import registerServiceWorker from './registerServiceWorker'
 
+ReactGA.initialize('UA-80844385-10')
+const logPageView = () => {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+logPageView()
