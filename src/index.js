4d7a6198f2079226752ad6cf40022275e7aee063
Arlindo Pereira <nighto@users.noreply.github.com>
2018-01-22 14:40:29 -0200

    removing mpemmapas specific Google Analytics code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 import App from './components/App/App.js'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactGA.initialize('UA-80844385-10')
+ReactGA.initialize('UA-XXXXXXXX-XX')
 const logPageView = () => {
     ReactGA.set({ page: window.location.pathname + window.location.search });
     ReactGA.pageview(window.location.pathname + window.location.search);
