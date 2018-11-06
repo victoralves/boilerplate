127fed450dafa6f54f0de9fc87a05a08a910b9c7
Gabriel Barbier <gabrielnasb@gmail.com>
2018-11-06 18:15:22 -0200

    Update project with Create React App v2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
-import './index.css'
-import App from './components/App/App.js'
-import registerServiceWorker from './registerServiceWorker'
-
-ReactGA.initialize('UA-XXXXXXXX-XX')
-const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname + window.location.search });
-    ReactGA.pageview(window.location.pathname + window.location.search);
-}
+import './index.sass'
+import App from './components/App/App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
-logPageView()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
